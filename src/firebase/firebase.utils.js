796cb5855c83a5e84c2de949d8d9010e7f95e1f8
vuehero18 <vuehero18@outlook.com>
2019-10-03 07:06:05 -0700

    Integrate firebase into app, Created  SignIn page

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyAkMmuU8hCgEmvBkwPo5ysMW-EBt-8VBDE",
+  authDomain: "crwn-db-36c14.firebaseapp.com",
+  databaseURL: "https://crwn-db-36c14.firebaseio.com",
+  projectId: "crwn-db-36c14",
+  storageBucket: "",
+  messagingSenderId: "125494534862",
+  appId: "1:125494534862:web:e9060a5c5b483f946ae69f",
+  measurementId: "G-XGM1X4BGK7"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
