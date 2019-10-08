14434ddd38683eec681f633a3ea6802528ee6763
vuehero18 <vuehero18@outlook.com>
2019-10-08 03:35:43 -0700

    Update Header, CustomButton, Hompage to have styled-component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -60,7 +60,7 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton onClick={signInWithGoogle} googleSignIn>
               Sign In with Google
             </CustomButton>
           </div>
